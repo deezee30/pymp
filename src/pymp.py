2737b407e21d059bc02abddbd4a1f9c68d297690
Deniss Zerkalijs <deezee30@outlook.com>
2021-02-04 01:34:55 +0000

    Introduce dev-key.json and skeleton of the bot

--- src/pymp.py
+++ src/pymp.py
@@ -1,42 +1,101 @@
-import os
-import argparse
+import os # Util
+import argparse # Command parser
+import time # Timing
+import json # JSON
+
+# python-binance lib
 from binance.enums import *
 from binance.client import Client
 from twisted.internet import reactor
 from binance.websockets import BinanceSocketManager
 from binance.exceptions import BinanceAPIException, BinanceOrderException
 
+DEV_KEY_FILE = "dev-key.json" # dev key file
+DEV_KEY_API = "api-key" # api-key identifier
+DEV_KEY_SECRET = "secret-key" # secret-key identifier
+TPS = 5 # ticks per second post-buy -> may also query binance at this rate
+
+def prompt_key_file():
+    """ Prompt to configure before usage, then quit 5s later. """
+
+    print("Please insert keys before usage. Exiting in 5s...")
+    time.sleep(5)
+    exit()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Pump n dump automated bot')
-    parser.add_argument("--public_key", type=str, default=None, required=True, help="Binance user API public key")
-    parser.add_argument("--private_key", type=str, default=None, required=True, help="Binance user API private key")
-    parser.add_argument("--btc", type=str, default=None, required=True, help="amount of BTC to use to purchase SHT")
-    parser.add_argument("--timer", type=int, default=0, required=True, help="Time to wait between buy and sell, in seconds")
-    parser.add_argument("--percentage", type=int, default=0, required=True, help="Percentage increase from buy price at which to sell")
+    parser.add_argument("--btc", type=str, default=None, required=True, help="amount of BTC to use to purchase coin")
+    parser.add_argument("--wait", type=int, default=0, required=True, help="Time to wait between buy and sell, in seconds")
+    parser.add_argument("--pct", type=int, default=0, required=False, help="Percentage increase from buy price at which to sell")
     args = parser.parse_args()
 
-    assert args.timer != 0, "ERROR: must specify a non zero value for --timer, use 'python pumpndump.py -h' for help"
-    btc = float(args.btc)
+    # Handle API key load
+    if os.path.exists(DEV_KEY_FILE):
+        # Load key file
+        with open(DEV_KEY_FILE, 'r') as f:
+            keys = json.loads(f.read())
+
+        # Prompt to configure before usage
+        if not keys[DEV_KEY_API] or not keys[DEV_KEY_SECRET]:
+            prompt_key_file()
+        
+        public_key = keys[DEV_KEY_API]
+        private_key = keys[DEV_KEY_SECRET]
+    else:
+        # Create blank keys
+        keys = {
+            DEV_KEY_API: "",
+            DEV_KEY_SECRET: ""
+        }
+
+        # Insert into new file
+        with open(DEV_KEY_FILE, 'w') as f:
+            json.dump(keys, f, indent=4)
+            print(f"Generated '{DEV_KEY_FILE}'")
+        
+        # Prompt to configure before usage
+        prompt_key_file()
+
+    assert args.timer != 0, "ERROR: must specify a non zero value for --timer, use 'python pymp.py -h' for help"
+    pump_btc = float(args.btc)
 
     # create new client obj
-    client = Client(args.public_key, args.private_key)
-
-    # change API endpoint for testing
-    # client.API_URL = "https://testnet.binance.vision/api"
+    client = Client(public_key, private_key)
+    print("Session initiated with Binance API")
 
     # get BTC balance and assert there is enough in account
     btc_acc_amt = float(client.get_asset_balance(asset="BTC")['free'])
-    assert btc_acc_amt >= btc, "ERROR: insufficient BTC funds in account, specify a smaller value for --btc"
+    assert btc_acc_amt >= pump_btc, "ERROR: insufficient BTC funds in account, specify a smaller value for --btc"
 
-    # Mode 1 (timer only)
-    # sell after specified time
-    if args.percentage == 0:
-        pass
+    # Wait for pump command
+    coin = input("Ready. Awaiting coin input: ").upper() # pump coin
+    symbol = f"{coin}BTC" # exchange symbol
+    pump_buy_t0 = int(round(time.time() * 1000)) # ms
+    pump_sell_t0 = pump_buy_t0 + args.wait * 1000 # ms
 
-    # Mode 2 (percentage/timer combo)
-    # sell if the inputted percentage increase or timer value is reached, whichever comes first
-    else:
-        pass
+    # Place order at market value using a balance quote
+    with client.order_market_buy(symbol=symbol,
+                                 quoteOrderQty=pump_btc) as order:
+        if order["status"] is "FILLED":
+            # TODO: Notify at which time the buy occurred
+            pump_buy_ms = order["transactTime"] - pump_buy_t0 # Time taken to buy in ms
+            executedQty = order["executedQty"]
+            print(f"Bought {executedQty} {coin} in {pump_buy_ms} ms.")
 
+            filled = True
+        else:
+            print(f"Order has not been filled. Response:")
+            print(order)
+    
+    if filled:
+        # Loop each tick at a TPS rate
+        while int(round(time.time() * 1000)) < pump_sell_t0:
+            pct_increase = 0 # TODO: Percentage monitoring
+            if pct_increase >= args.pct:
+                # Sell
+                break
 
-
+            time.sleep(1/TPS)
+        
+        # Sell as soon as either condition is reached
+        # TODO: Execute sell
